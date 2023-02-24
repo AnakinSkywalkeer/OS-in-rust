2400ea2ed82c7177fe8bd5dbf4cbc4377d63bd21
AnakinSkywalkeer <gurmanavdeepsinghmahal9@gmail.com>
2023-02-24 21:16:18 +0530

    added the safe way to write smth to the vga

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,9 @@ fn panic(_info:&PanicInfo)-> !{
 
 #[no_mangle]
 pub extern "C" fn _start()-> !{
-    vga_buffer::print_something();
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again Gary this side").unwrap();
+    write!(vga_buffer::WRITER.lock(),"Some numbers {} {}",0.78,90).unwrap();
     loop{}
 }
 
