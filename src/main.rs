3687d48e2408d41d0e59dd1ae23e7ed08167e3e0
AnakinSkywalkeer <gurmanavdeepsinghmahal9@gmail.com>
2023-02-24 16:38:18 +0530

    added some changes like fixed the memory location for vba buffer

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn panic(_info:&PanicInfo)-> !{
     loop{}
 }
 
-static HELLO:&[u8]=b"Hello world Gary this side";
+
 #[no_mangle]
 pub extern "C" fn _start()-> !{
     vga_buffer::print_something();
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,6 @@
+use volatile::Volatile;
+use core::fmt;
+
 #[allow(dead_code)]
 #[derive(Debug,Clone,Copy,PartialEq,Eq)]
 #[repr(u8)]
@@ -40,7 +43,7 @@ const  BUFFER_HEIGHT:usize=80;
 
 #[repr(transparent)]
 struct Buffer{
-    chars:[[ScreenChar;BUFFER_WIDTH];BUFFER_HEIGHT]
+    chars:[[Volatile<ScreenChar>;BUFFER_WIDTH];BUFFER_HEIGHT]
 }
 
 pub struct Writer{
@@ -61,10 +64,10 @@ impl Writer{
                 let col=self.column_position;
 
                 let color_code=self.color_code;
-                self.buffer.chars[row][col]=ScreenChar{
+                self.buffer.chars[row][col].write(ScreenChar{
                     ascii_character:byte,
                     color_code,
-                };
+                });
                 self.column_position+=1;
             }
         }
@@ -72,23 +75,29 @@ impl Writer{
     fn write_string(&mut self , s:&str){
         for byte in s.bytes(){
             match byte{
-                0x20..=0x7e | b'n'=>self.write_byte(byte),
+                0x20..=0x7e | b'\n'=>self.write_byte(byte),
                 _ => self.write_byte(0xfe),
             }
         }
     }
     fn new_line(&mut self){/*1000*/}
 }
-
+impl fmt::Write for Writer{
+    fn write_str(&mut self,s:&str)->fmt::Result{
+        self.write_string(s);
+        Ok(())
+    }
+}
 pub fn print_something(){
+    use core::fmt::Write;
     let mut writer=Writer{
         column_position:0,
         color_code:ColorCode::new(Color::Yellow,Color::Black),
         buffer:unsafe{
-            &mut *(0xb000 as *mut Buffer)
+            &mut *(0xb8000 as *mut Buffer)
         }
     };
     writer.write_byte(b'H');
     writer.write_string("ello ");
-    writer.write_string("World!");
+    write!(writer,"hmmm {} {}",0.223,0.22).unwrap();
 }
