73f0d62c0a5aa5965c0321d5b543046a3e2e6429
AnakinSkywalkeer <gurmanavdeepsinghmahal9@gmail.com>
2023-02-25 20:25:17 +0530

    added the boot.rs and lib.rs to make the structure better

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,34 @@
+// in src/main.rs
+
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = "tes"]
-mod serial;
-mod vga_buffer;
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
-use core::fmt::Write;
+use blog_os::println;
 
-#[derive(Debug,Clone,Copy,PartialEq,Eq)]
-#[repr(u32)]
-pub enum QemuExitCode{
-    Success=0x10,
-    Failed=0x11,
-}
-pub trait Testable{
-    fn run(&self)->();
-}
-
-impl<T> Testable for T
-where
-   T:Fn()
-{
-    fn run(&self){
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-pub fn exit_qemu(exit_code:QemuExitCode){
-    use x86_64::instructions::port::Port;
-
-    unsafe{
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-#[cfg(not(test))] 
-#[panic_handler]
-fn panic(info:&PanicInfo)-> !{
-    println!("{}",info);
-    loop{}
-}
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    println!("Hello world");
 
     #[cfg(test)]
-    tes();
-    
-    println!("It did not crash!!!");
+    test_main();
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-    
-}
-// our panic handler in test mode
-
-
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -140,22 +140,22 @@ pub fn _print(args:fmt::Arguments){
 
 
 
-#[test_case]
-fn test_println_simple(){
-    println!("test_println_simple");
-}
-#[test_case]
-fn test_println_many(){
-    for _ in 0..200{
-        println!("Hello world");
-    }
-}
-#[test_case]
-fn test_println_output(){
- let s="Some random string";
- println!("{}",s);
- for (i,c) in s.chars().enumerate(){
-    let screen_char=WRITER.lock().buffer.chars[BUFFER_HEIGHT-2][i].read();
-    assert_eq!(char::from(screen_char.ascii_character),c);
- }
-}
+// #[test_case]
+// fn test_println_simple(){
+//     println!("test_println_simple");
+// }
+// #[test_case]
+// fn test_println_many(){
+//     for _ in 0..200{
+//         println!("Hello world");
+//     }
+// }
+// #[test_case]
+// fn test_println_output(){
+//  let s="Some random string";
+//  println!("{}",s);
+//  for (i,c) in s.chars().enumerate(){
+//     let screen_char=WRITER.lock().buffer.chars[BUFFER_HEIGHT-2][i].read();
+//     assert_eq!(char::from(screen_char.ascii_character),c);
+//  }
+// }
